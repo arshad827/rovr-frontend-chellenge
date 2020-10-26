f7fa93af3b634bbb63565b8abb903cf1a4a9d577
BT-Mohammed <arshad@shroomscreatives.in>
2020-10-26 09:10:54 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+// Two routes for SearchComponent, DashboardComponent
+const routes: Routes = [
+  { path: '', component: SearchComponent },
+  { path: 'dashboard', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
